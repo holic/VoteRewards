17e9f522c02dd4f6cd3903c6be200795dcfb3fb7
Kevin Ingersoll <kingersoll@gmail.com>
2012-08-28 23:42:31 -0700

    Initial commit

---
+++ src/com/levelupmc/voterewards/Plugin.java
@@ -0,0 +1,115 @@
+package com.levelupmc.voterewards;
+
+import java.util.logging.Level;
+import java.text.MessageFormat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class Plugin extends JavaPlugin {
+    // TODO: configurable Redis host
+    protected static JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+    
+    // TODO: use REST API instead of a direct connection to Redis?
+    
+    @Override
+    public void onEnable() {
+        
+        getCommand("claim").setExecutor(new CommandExecutor() {
+            public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+                if(sender instanceof Player) {
+                    rewardPlayer((Player) sender);
+                }
+                else {
+                    sender.sendMessage("You must be a Player to execute this command.");
+                }
+                return true;
+            }
+        });
+        
+        // TODO: trigger reminder on login and at interval after login
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            public void run() {
+                for(Player player : Bukkit.getOnlinePlayers()) {
+                    remindPlayer(player);
+                }
+            }
+        }, 20*60*2, 20*60*2);// TODO: configurable delay
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        // nothing to do!
+    }
+    
+    
+    public void rewardPlayer(Player player) {
+        Jedis redis = pool.getResource();
+        
+        // TODO: configurable redis key
+        String key = "votifier:credits:" + player.getName().toLowerCase();
+
+        if(redis.lpop(key) == null) {
+            // TODO: configurable message
+            player.sendMessage("You don't have any vote credits to claim.");
+        }
+        else {
+            
+            
+            
+            Long credits = redis.llen(key);
+            // TODO: configurable messages
+            player.sendMessage(credits > 0
+                    ? "Claimed! You have " + credits + " credits left."
+                    : "Claimed! Don't forget to vote tomorrow!");
+        }
+        
+        pool.returnResource(redis);
+        log("{0} claimed a reward.", player.getName());
+    }
+    
+    
+    public void remindPlayer(Player player) {
+        Jedis redis = pool.getResource();
+        
+        Double lastClaim = redis.zscore("votifier:claims", player.getName().toLowerCase());
+        // TODO: configurable timeout
+        if(lastClaim == null || (System.currentTimeMillis() / 1000L) - lastClaim > 60 * 60 * 18) {
+            // TODO: configurable message
+            player.sendMessage("You still need to vote today to receive your daily reward!");
+        }
+
+        pool.returnResource(redis);
+    }
+    
+    
+    public void log(String message) {
+        log(Level.INFO, message);
+    }
+    public void log(Level level, String message) {
+        getLogger().log(level, message);
+    }
+    public void log(String message, Object... args) {
+        log(MessageFormat.format(message, args));
+    }
+    public void log(Level level, String message, Object... args) {
+        log(level, MessageFormat.format(message, args));
+    }
+    
+    public void debug(String message) {
+        log(Level.FINE, message);
+    }
+    public void debug(String message, Object... args) {
+        debug(MessageFormat.format(message, args));
+    }
+}
